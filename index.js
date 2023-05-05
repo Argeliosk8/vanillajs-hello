288b7e219bb3ef80db24da6fa7f0124bae10a717
Argeliosk8 <argelio.baca@gmail.com>
2023-05-05 16:57:13 -0600

    my first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//document.getElementById("excuse").innerHTML = "I have changed";
+
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+function onLoad(){
+    let selectedWho = who[Math.floor(Math.random() * (who.length - 1) + 1)];
+    let selectedAction = action[Math.floor(Math.random() * (action.length - 1) + 1)];
+    let selectedWhat = what[Math.floor(Math.random() * (what.length - 1) + 1)];
+    let selectedWhen = when[Math.floor(Math.random() * (when.length - 1) + 1)];
+    return `${selectedWho} ${selectedAction} ${selectedWhat} ${selectedWhen}`
+}
+
+document.getElementById("excuse").innerHTML = onLoad()
